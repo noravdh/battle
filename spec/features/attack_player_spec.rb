c2b7675e53352c37d3a50b507f22a2f96d6b8e4b
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-04 15:32:13 +0000

    implemented hit points

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -4,4 +4,12 @@ feature 'Attacking' do
     click_button 'Attack'
     expect(page).to have_content "Pikachu attacked Snorlax"
   end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    # click_link 'OK'
+    expect(page).not_to have_content 'Snorlax: 60HP'
+    expect(page).to have_content 'Snorlax: 50HP'
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,30 @@
 require 'player'
 
 describe Player do
+  subject(:pikachu) {Player.new('Pikachu')}
+  subject(:snorlax) {Player.new('Snorlax')}
+
   it "can return it's name" do
     player = Player.new("Pikachu") 
     expect(player.name).to eq "Pikachu"
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(pikachu.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(snorlax).to receive(:receive_damage)
+      pikachu.attack(snorlax)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { pikachu.receive_damage }.to change {pikachu.hit_points}.by(-10)
+    end
   end
 end
